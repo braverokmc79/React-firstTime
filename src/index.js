083b3e5d9eb3714c91eb90e41c154e8701bb5f7f
braverokmc79 <braverokmc79@gmail.com>
2022-08-23 19:16:29 +0900

    8.출석부 출력하기

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import NotificationList from './chapter_06/NotificationList';
 import Accommodate from './chapter_07/Accommodate';
 import ConfirmButton from './chapter_08/ConfirmButton';
 import LandingPage from './chapter_09/LandingPage';
+import AttendanceBook from './chapter_10/AttendanceBook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -21,12 +22,9 @@ root.render(
     {/* <NotificationList /> */}
     {/* <Accommodate /> */}
     {/* <ConfirmButton /> */}
-    <LandingPage />
+    {/* <LandingPage /> */}
+    <AttendanceBook />
   </React.StrictMode>
-
-
 );
 
 reportWebVitals();
-
-
