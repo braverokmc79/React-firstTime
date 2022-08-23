7a82cfec78039e0b4758ff31e19a566fd410e1a7
braverokmc79 <braverokmc79@gmail.com>
2022-08-23 18:48:43 +0900

    7.로그인 여부를 나타내는 툴바 만들기

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
 import Accommodate from './chapter_07/Accommodate';
 import ConfirmButton from './chapter_08/ConfirmButton';
+import LandingPage from './chapter_09/LandingPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,7 +20,8 @@ root.render(
     {/* <CommentList /> */}
     {/* <NotificationList /> */}
     {/* <Accommodate /> */}
-    <ConfirmButton />
+    {/* <ConfirmButton /> */}
+    <LandingPage />
   </React.StrictMode>
 
 
